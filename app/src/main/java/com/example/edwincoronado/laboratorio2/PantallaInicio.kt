4754b582c94bd60b88f2c15b9d9835f753fcd9eb
Edwin Coronado <cor14148@uvg.edu.gt>
2019-01-16 07:54:32 -0600

    Initial commit

---
+++ app/src/main/java/com/example/edwincoronado/laboratorio2/PantallaInicio.kt
@@ -0,0 +1,12 @@
+package com.example.edwincoronado.laboratorio2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PantallaInicio : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_inicio)
+    }
+}
